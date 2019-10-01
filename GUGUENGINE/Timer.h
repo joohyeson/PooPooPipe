97a8925b69bdcc22ea8ad5367d9f381ad12a5ce9
ruru <digipen.hagyeong@gmail.com>
2019-10-01 20:34:46 +0900

    real

---
+++ GUGUENGINE/Timer.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <chrono>
+
+class Timer {
+
+public:
+
+	Timer();
+	~Timer();
+
+	void start(void);
+	void stop(void);
+
+	void SetTime(void);
+	double GetDT(void);
+	double GetTime(void);
+
+private:
+
+	std::chrono::time_point<std::chrono::system_clock> startTime;
+	std::chrono::time_point<std::chrono::system_clock> previousTime;
+	std::chrono::time_point<std::chrono::system_clock> currentTime;
+	std::chrono::time_point<std::chrono::system_clock> endTime;
+
+	bool  IsRunning = false;
+};
