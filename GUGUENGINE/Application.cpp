c6aa4265fa2508f83739b53d960a04259830765b
ruru <digipen.hagyeong@gmail.com>
2019-10-17 01:56:05 +0900

    to engine proof presentation total version

--- GUGUENGINE/Application.cpp
+++ GUGUENGINE/Application.cpp
@@ -15,66 +15,67 @@ GLuint mVertexArrayObject2;
 GLuint mPositionVertexBufferObjectID2, mColorVertexBufferObjectID2;
 GLuint triangleTextureCoordinateBufferObjectID2;
 Shader mShader2;
- float time = 0;
+
+GLuint mVertexArrayObject3;
+GLuint mPositionVertexBufferObjectID3, mColorVertexBufferObjectID3;
+GLuint triangleTextureCoordinateBufferObjectID3;
+Shader mShader3;
+
 #define WIDTH 800
 #define HEIGHT 800
+
+int cheche = 0;
 int moveCheck = 0;
+float time = 0;
+int rotationCheck = 0;
+
 glm::vec3 curser = { 0, 0, 0 };
 glm::vec3 getOrigin = { 0, 0, 0 };
 glm::vec3 getOrigin2 = { 0, 0, 0 };
 
 float r = 0.f;
-int rotationCheck = 0;
-//Mesh mMesh = MESH::create_rectangle();
-//Mesh mMesh = MESH::create_box();
-//Mesh mMesh = MESH::create_triangle({ -0.5f, -0.5f, 1.0f }, { 0.5f, -0.5f, 1.0f }, { 0.0f, 0.5f, 1.0f });
 
-Mesh mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { 0, 0, 0 }, time);
-Mesh mMesh2 = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { 0.4,0.3,0 }, 0);
+Mesh mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { 0.1, 0.7, 1 }, glm::radians(0.f));
+Mesh mMesh2 = MESH::create_box(0.4f, { 255, 255, 255 });
+Mesh mMesh3 = MESH::create_circle(0, { 255, 255, 255 }, 6, { 0.1, 0.7, 1 }, glm::radians(0.f));
+
+
+float textureCoordinate[] = { 0.5f, 0.5f,
+						0.5f, 0.0f,
+						1.0f, 0.25f ,
+						1.0f, 0.75f,
+						0.5f, 1.0f,
+						0.0f, 0.75f,
+						0.0f, 0.25f,
+						0.5f, 0.0f };
+
 
 GLuint CreateTexture(char const* filename, int i)
 {
-	// Determine the format of the image.
-	// Note: The second paramter ('size') is currently unused, and we should use 0 for it.
 	FREE_IMAGE_FORMAT format = FreeImage_GetFileType(filename, 0);
 
-	// Image not found? Abort! Without this section we get a 0 by 0 image with 0 bits-per-pixel but we don't abort, which
-	// you might find preferable to dumping the user back to the desktop.
 	if (format == -1)
 	{
 		std::cout << "Could not find image: " << filename << " - Aborting." << std::endl;
 		exit(-1);
 	}
 
-	// Found image, but couldn't determine the file format? Try again...
 	if (format == FIF_UNKNOWN)
 	{
 		std::cout << "Couldn't determine file format - attempting to get from file extension..." << std::endl;
-
-		// ...by getting the filetype from the filename extension (i.e. .PNG, .GIF etc.)
-		// Note: This is slower and more error-prone that getting it from the file itself,
-		// also, we can't use the 'U' (unicode) variant of this method as that's Windows only.
 		format = FreeImage_GetFIFFromFilename(filename);
-
-		// Check that the plugin has reading capabilities for this format (if it's FIF_UNKNOWN,
-		// for example, then it won't have) - if we can't read the file, then we bail out =(
 		if (!FreeImage_FIFSupportsReading(format))
 		{
 			std::cout << "Detected image format cannot be read!" << std::endl;
 			exit(-1);
 		}
 	}
-
-	// If we're here we have a known image format, so load the image into a bitap
 	FIBITMAP* bitmap = FreeImage_Load(format, filename);
-
-	// How many bits-per-pixel is the source image?
 	int bitsPerPixel = FreeImage_GetBPP(bitmap);
 
 	FIBITMAP* bitmap32;
 	if (bitsPerPixel == 32)
 	{
-		//std::cout << "Source image has " << bitsPerPixel << " bits per pixel. Skipping conversion." << std::endl;
 		bitmap32 = bitmap;
 	}
 	else
@@ -83,29 +84,24 @@ GLuint CreateTexture(char const* filename, int i)
 		bitmap32 = FreeImage_ConvertTo32Bits(bitmap);
 	}
 
-	// Some basic image info - strip it out if you don't care
 	int imageWidth = FreeImage_GetWidth(bitmap32);
 	int imageHeight = FreeImage_GetHeight(bitmap32);
-	//std::cout << "Image: " << filename << " is size: " << imageWidth << "x" << imageHeight << "." << std::endl;
 
 	GLubyte* textureData = FreeImage_GetBits(bitmap32);
 
-	// Generate a texture ID and bind to it
 	GLuint tempTextureID;
 	glGenTextures(1, &tempTextureID);
 	glBindTexture(GL_TEXTURE_2D, tempTextureID);
 
-	glTexImage2D(GL_TEXTURE_2D,    // Type of texture
-		0,                // Mipmap level (0 being the top level i.e. full size)
-		GL_RGBA,          // Internal format
-		imageWidth,       // Width of the texture
-		imageHeight,      // Height of the texture,
-		0,                // Border in pixels
-		GL_BGRA,          // Data format
-		GL_UNSIGNED_BYTE, // Type of texture data
-		textureData);     // The image data to use for this texture
-
-						  // Specify our minification and magnification filters
+	glTexImage2D(GL_TEXTURE_2D,
+		0,
+		GL_RGBA,
+		imageWidth,
+		imageHeight,
+		0,
+		GL_BGRA,
+		GL_UNSIGNED_BYTE,
+		textureData);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -116,8 +112,6 @@ GLuint CreateTexture(char const* filename, int i)
 
 	glBindTexture(GL_TEXTURE_2D, i);
 
-
-	// Check for OpenGL texture creation errors
 	GLenum glError = glGetError();
 	if (glError)
 	{
@@ -144,36 +138,18 @@ GLuint CreateTexture(char const* filename, int i)
 		std::cout << "See https://www.opengl.org/sdk/docs/man/html/glTexImage2D.xhtml for further details." << std::endl;
 	}
 
-	// Unload the 32-bit colour bitmap
 	FreeImage_Unload(bitmap32);
-
-	// If we had to do a conversion to 32-bit colour, then unload the original
-	// non-32-bit-colour version of the image data too. Otherwise, bitmap32 and
-	// bitmap point at the same data, and that data's already been free'd, so
-	// don't attempt to free it again! (or we'll crash).
 	if (bitsPerPixel != 32)
 	{
 		FreeImage_Unload(bitmap);
 	}
 
-	// Finally, return the texture ID
 	return tempTextureID;
 }
 
 
 bool defineVertexArrayObject() {
 
-	float textureCoordinate[] = { 0.5f, 0.5f,
-							0.5f, 0.0f,
-							1.0f, 0.25f ,
-							1.0f, 0.75f,
-							0.5f, 1.0f,
-							0.0f, 0.75f,
-							0.0f, 0.25f,
-							0.5f, 0.0f };
-
-	//#2
-	//Vertex Buffer Object(VBO)를 생성하여 vertex 데이터를 복사한다.
 	glGenBuffers(1, &mPositionVertexBufferObjectID);
 	glBindBuffer(GL_ARRAY_BUFFER, mPositionVertexBufferObjectID);
 	glBufferData(GL_ARRAY_BUFFER, mMesh.GetPointCount() * sizeof(float) * 3, &mMesh.GetPoint()[0], GL_STATIC_DRAW);
@@ -182,7 +158,6 @@ bool defineVertexArrayObject() {
 	glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoordinate), textureCoordinate, GL_STATIC_DRAW);
 
-	//#6
 	glGenVertexArrays(1, &mVertexArrayObject);
 	glBindVertexArray(mVertexArrayObject);
 
@@ -212,18 +187,6 @@ bool defineVertexArrayObject() {
 }
 
 bool defineVertexArrayObject2() {
-
-	float textureCoordinate[] = { 0.5f, 0.5f,
-							0.5f, 0.0f,
-							1.0f, 0.25f ,
-							1.0f, 0.75f,
-							0.5f, 1.0f,
-							0.0f, 0.75f,
-							0.0f, 0.25f,
-							0.5f, 0.0f };
-
-	//#2
-	//Vertex Buffer Object(VBO)를 생성하여 vertex 데이터를 복사한다.
 	glGenBuffers(1, &mPositionVertexBufferObjectID2);
 	glBindBuffer(GL_ARRAY_BUFFER, mPositionVertexBufferObjectID2);
 	glBufferData(GL_ARRAY_BUFFER, mMesh2.GetPointCount() * sizeof(float) * 3, &mMesh2.GetPoint()[0], GL_STATIC_DRAW);
@@ -232,7 +195,6 @@ bool defineVertexArrayObject2() {
 	glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID2);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoordinate), textureCoordinate, GL_STATIC_DRAW);
 
-	//#6
 	glGenVertexArrays(1, &mVertexArrayObject2);
 	glBindVertexArray(mVertexArrayObject2);
 
@@ -262,11 +224,46 @@ bool defineVertexArrayObject2() {
 	return true;
 }
 
+bool defineVertexArrayObject3() {
+	glGenBuffers(1, &mPositionVertexBufferObjectID3);
+	glBindBuffer(GL_ARRAY_BUFFER, mPositionVertexBufferObjectID3);
+	glBufferData(GL_ARRAY_BUFFER, mMesh3.GetPointCount() * sizeof(float) * 3, &mMesh3.GetPoint()[0], GL_STATIC_DRAW);
+
+	glGenBuffers(1, &triangleTextureCoordinateBufferObjectID3);
+	glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoordinate), textureCoordinate, GL_STATIC_DRAW);
+
+	//#6
+	glGenVertexArrays(1, &mVertexArrayObject3);
+	glBindVertexArray(mVertexArrayObject3);
+
+
+	GLint positionAttribute3 = glGetAttribLocation(mShader3.GetShaderID(), "positionAttribute");
+	if (positionAttribute3 == -1) {
+		std::cerr << "position 속성 설정 실패" << std::endl;
+		return false;
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, mPositionVertexBufferObjectID3);
+	glVertexAttribPointer(positionAttribute3, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glEnableVertexAttribArray(positionAttribute3);
+
+	GLint textureCoordinateAttribute3 = glGetAttribLocation(mShader3.GetShaderID(), "textureCoordinateAttribute");
+	if (textureCoordinateAttribute3 == -1) {
+		std::cerr << "Texture Coordinate 속성 설정 실패" << std::endl;
+		return false;
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID3);
+	glVertexAttribPointer(textureCoordinateAttribute3, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(textureCoordinateAttribute3);
+
+	glBindVertexArray(0);
+
+	return true;
+}
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
-	//처음 2개의 파라미터는 viewport rectangle의 왼쪽 아래 좌표
-	//다음 2개의 파라미터는 viewport의 너비와 높이이다.
-	//framebuffer의 width와 height를 가져와 glViewport에서 사용한다.
 	glViewport(0, 0, width, height);
 
 	framebufferWidth = width;
@@ -280,13 +277,38 @@ void errorCallback(int errorCode, const char* errorDescription)
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	
 	double xpos, ypos;
-	//getting cursor position
 	glfwGetCursorPos(window, &xpos, &ypos);
 	std::cout << "Cursor Position at (" << xpos << " : " << ypos << std::endl;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+	/*if (key == GLFW_KEY_D)
+	{
+		time += 0.3f;
+		std::cout << "TIME:" << time << std::endl;
+	}*/
+
+	if (key == GLFW_KEY_SPACE)
+	{
+		//mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { 0.1, 0.7, 1 }, 0);
+		if (cheche < 2)
+		{
+			mMesh2 = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { 0, 0, 1 }, 0);
+		}
+
+		if (cheche >= 2)
+		{
+			mMesh2 = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { -0.3, 0.3, 1 }, 0);
+		}
+
+		if (cheche > 3)
+		{
+			mMesh3 = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { 0.22f, 0.3, 1 }, 0);
+		}
+
+		cheche++;
+	}
 
 }
 
@@ -300,30 +322,26 @@ void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
 
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-	
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	if (cheche >= 1)
 	{
-		moveCheck += 1;
-		std::cout << "Left mouse button pressed" << std::endl;
-		
-		std::cout << "TIME:" << time << std::endl;
-		//mMesh = MESH::create_circle(0.7f, { 255, 255, 255 }, 6, { 400, 300, 0 }, time);
+		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+		{
+			moveCheck += 1;
+			std::cout << "Left mouse button pressed" << std::endl;
+			time += 0.9f;
+			std::cout << "TIME:" << time << std::endl;
+		}
+		if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+		{
+			time += glm::radians(60.f);
+			rotationCheck = 1;
+		}
 	}
-	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-	{
-		time += 0.9f;
-		rotationCheck = 1;
-	}
-
 }
 
 
 int main()
 {
-
-	//glfwSetErrorCallback(errorCallback);
-
-	//Shader mShader();
 	if (!glfwInit()) {
 
 		std::cerr << "Error: GLFW 초기화 실패" << std::endl;
@@ -371,8 +389,8 @@ int main()
 
 
 
-	if (!GLEW_VERSION_3_3) {
-
+	if (!GLEW_VERSION_3_3)
+	{
 		std::cerr << "Error: OpenGL 3.3 API is not available." << std::endl;
 
 		glfwTerminate();
@@ -401,6 +419,13 @@ int main()
 		std::exit(EXIT_FAILURE);
 	}
 
+	if (!mShader3.LoadFromSource()) {
+
+		std::cerr << "Error: Shader Program 생성 실패" << std::endl;
+
+		glfwTerminate();
+		std::exit(EXIT_FAILURE);
+	}
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -412,12 +437,32 @@ int main()
 			std::exit(EXIT_FAILURE);
 		}
 
-
 		glBindVertexArray(mVertexArrayObject2);
 
-		GLint tex2Loc = glGetUniformLocation(mShader2.GetShaderID(), "tex");
-		glUniform1i(tex2Loc, 1);
-		glActiveTexture(GL_TEXTURE1);
+		if (cheche == 0)
+		{
+			GLint texLoc2 = glGetUniformLocation(mShader2.GetShaderID(), "tex");
+			glUniform1i(texLoc2, 0);
+			glDrawArrays(mMesh.GetPointListPattern(), 0, 8);
+		}
+		else if (cheche == 1)
+		{
+			GLuint texureId2 = CreateTexture("assets\\image0.png", 0);
+			GLint texLoc2 = glGetUniformLocation(mShader2.GetShaderID(), "tex");
+			glUniform1i(texLoc2, 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texureId2);
+			glDrawArrays(mMesh.GetPointListPattern(), 0, 8);
+		}
+		else if (cheche > 2)
+		{
+			GLuint texureId2 = CreateTexture("assets\\image1.png", 0);
+			GLint texLoc2 = glGetUniformLocation(mShader2.GetShaderID(), "tex");
+			glUniform1i(texLoc2, 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texureId2);
+			glDrawArrays(mMesh2.GetPointListPattern(), 0, 8);
+		}
 
 		glDrawArrays(mMesh2.GetPointListPattern(), 0, 8);
 
@@ -432,17 +477,42 @@ int main()
 		glUseProgram(mShader.GetShaderID());
 		glBindVertexArray(mVertexArrayObject);
 
-		GLuint texureId = CreateTexture("assets\\image0.png", 0);
+		if (cheche == 0)
+		{
+			GLint texLoc = glGetUniformLocation(mShader.GetShaderID(), "tex");
+			glUniform1i(texLoc, 0);
+			glDrawArrays(mMesh.GetPointListPattern(), 0, 8);
+		}
+		else
+		{
+			GLuint texureId = CreateTexture("assets\\image0.png", 0);
+			GLint texLoc = glGetUniformLocation(mShader.GetShaderID(), "tex");
+			glUniform1i(texLoc, 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texureId);
+			glDrawArrays(mMesh.GetPointListPattern(), 0, 8);
+		}
 
-		GLint texLoc = glGetUniformLocation(mShader.GetShaderID(), "tex");
-		glUniform1i(texLoc, 0);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texureId);
-		glDrawArrays(mMesh.GetPointListPattern(), 0, 8);
+		if (!defineVertexArrayObject3()) {
 
+			std::cerr << "Error: Shader Program 3" << std::endl;
 
+			glfwTerminate();
+			std::exit(EXIT_FAILURE);
+		}
 
+		glUseProgram(mShader3.GetShaderID());
+		glBindVertexArray(mVertexArrayObject3);
 
+		if (cheche > 3)
+		{
+			GLuint texureId3 = CreateTexture("assets\\image2.png", 0);
+			GLint texLoc3 = glGetUniformLocation(mShader3.GetShaderID(), "tex");
+			glUniform1i(texLoc3, 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texureId3);
+			glDrawArrays(mMesh3.GetPointListPattern(), 0, 8);
+		}
 
 		double lastTime = glfwGetTime();
 		int numOfFrames = 0;
@@ -456,63 +526,59 @@ int main()
 			numOfFrames = 0;
 			lastTime = currentTime;
 		}
-
-		//glfwSwapInterval(1);
 		glfwSwapBuffers(window);
 
 		glClearColor(255, 255, 255, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-
-
-
-		getOrigin.x = mMesh.origin.x;
-		getOrigin.y = mMesh.origin.y;
-
-		getOrigin2.x = mMesh2.origin.x;
-		getOrigin2.y = mMesh2.origin.y;
-
-		r = mMesh.radius_r;
-
-
-		if (curser.x <= (getOrigin.x + r / 2) &&
-			curser.x >= (getOrigin.x - r / 2) &&
-			curser.y <= (getOrigin.y + r) &&
-			curser.y >= (getOrigin.y - r))
+		if (cheche > 1)
 		{
-			if (moveCheck % 2 == 1)
+			getOrigin.x = mMesh.origin.x;
+			getOrigin.y = mMesh.origin.y;
+
+			getOrigin2.x = mMesh2.origin.x;
+			getOrigin2.y = mMesh2.origin.y;
+
+			r = mMesh.radius_r;
+
+
+			if (curser.x <= (getOrigin.x + r / 2) &&
+				curser.x >= (getOrigin.x - r / 2) &&
+				curser.y <= (getOrigin.y + r) &&
+				curser.y >= (getOrigin.y - r))
 			{
-				mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { curser.x, curser.y ,0 }, time);
-			}
-		}
-		else
-		{
-			moveCheck = 0;
-		}
-
-		if (getOrigin.x <= (getOrigin2.x + r / 2) &&
-			getOrigin.x >= (getOrigin2.x - r / 2) &&
-			getOrigin.y <= (getOrigin2.y + r) &&
-			getOrigin.y >= (getOrigin2.y - r))
-		{
-			if (moveCheck % 2 == 0)
-			{
-				mMesh = mMesh2;
-				if (rotationCheck == 1)
+				if (moveCheck % 2 == 1)
 				{
-					mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { getOrigin.x, getOrigin.y ,1}, time);
-					mMesh2 = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, {0.4f, 0.3f,1 }, time);
-					rotationCheck = 0;
+					mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { curser.x, curser.y ,0 }, 0);
+				}
+			}
+			else
+			{
+				moveCheck = 0;
+			}
+
+			if (getOrigin.x <= (getOrigin2.x + r / 2) &&
+				getOrigin.x >= (getOrigin2.x - r / 2) &&
+				getOrigin.y <= (getOrigin2.y + r) &&
+				getOrigin.y >= (getOrigin2.y - r))
+			{
+				if (moveCheck % 2 == 0)
+				{
+					mMesh = mMesh2;
+					if (rotationCheck == 1)
+					{
+						mMesh = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { -0.3, 0.3, 1 }, time);
+						std::cout << time;
+						mMesh2 = MESH::create_circle(0.3f, { 255, 255, 255 }, 6, { -0.3, 0.3, 1 }, time);
+						rotationCheck = 0;
+					}
 				}
 			}
 		}
-
 		count++;
-
-		//glfwSwapBuffers(window);
 		glfwPollEvents();
-
 	}
+
 	glUseProgram(0);
 	glBindVertexArray(0);
 
