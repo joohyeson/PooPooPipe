151e7b80f61327e350df42750d0cdaed56913537
joohyeson <juhye.son990902@gmail.com>
2019-10-16 15:44:02 +0900

    popUP eunjin's pipe image

--- GUGUENGINE/Application.cpp
+++ GUGUENGINE/Application.cpp
@@ -3,10 +3,12 @@
 #include "external/glfw/include/GLFW/glfw3.h"
 #include "Mesh.h"
 #include "Shader.h"
+#include "FreeImage.h"
 
 int framebufferWidth, framebufferHeight;
 GLuint mVertexArrayObject;
 GLuint mPositionVertexBufferObjectID, mColorVertexBufferObjectID;
+GLuint triangleTextureCoordinateBufferObjectID;
 Shader mShader;
 #define WIDTH 800
 #define HEIGHT 600
@@ -19,19 +21,192 @@ glm::vec3 curser = { 0, 0, 0 };
 
 Mesh mMesh = MESH::create_circle(0.7f, { 255, 255, 255 }, 6, { 400, 300, 0 }, 0);
 
+
+GLuint CreateTexture(char const* filename)
+{
+	// Determine the format of the image.
+	// Note: The second paramter ('size') is currently unused, and we should use 0 for it.
+	FREE_IMAGE_FORMAT format = FreeImage_GetFileType(filename, 0);
+
+	// Image not found? Abort! Without this section we get a 0 by 0 image with 0 bits-per-pixel but we don't abort, which
+	// you might find preferable to dumping the user back to the desktop.
+	if (format == -1)
+	{
+		std::cout << "Could not find image: " << filename << " - Aborting." << std::endl;
+		exit(-1);
+	}
+
+	// Found image, but couldn't determine the file format? Try again...
+	if (format == FIF_UNKNOWN)
+	{
+		std::cout << "Couldn't determine file format - attempting to get from file extension..." << std::endl;
+
+		// ...by getting the filetype from the filename extension (i.e. .PNG, .GIF etc.)
+		// Note: This is slower and more error-prone that getting it from the file itself,
+		// also, we can't use the 'U' (unicode) variant of this method as that's Windows only.
+		format = FreeImage_GetFIFFromFilename(filename);
+
+		// Check that the plugin has reading capabilities for this format (if it's FIF_UNKNOWN,
+		// for example, then it won't have) - if we can't read the file, then we bail out =(
+		if (!FreeImage_FIFSupportsReading(format))
+		{
+			std::cout << "Detected image format cannot be read!" << std::endl;
+			exit(-1);
+		}
+	}
+
+	// If we're here we have a known image format, so load the image into a bitap
+	FIBITMAP* bitmap = FreeImage_Load(format, filename);
+
+	// How many bits-per-pixel is the source image?
+	int bitsPerPixel = FreeImage_GetBPP(bitmap);
+
+	// Convert our image up to 32 bits (8 bits per channel, Red/Green/Blue/Alpha) -
+	// but only if the image is not already 32 bits (i.e. 8 bits per channel).
+	// Note: ConvertTo32Bits returns a CLONE of the image data - so if we
+	// allocate this back to itself without using our bitmap32 intermediate
+	// we will LEAK the original bitmap data, and valgrind will show things like this:
+	//
+	// LEAK SUMMARY:
+	//  definitely lost: 24 bytes in 2 blocks
+	//  indirectly lost: 1,024,874 bytes in 14 blocks    <--- Ouch.
+	//
+	// Using our intermediate and cleaning up the initial bitmap data we get:
+	//
+	// LEAK SUMMARY:
+	//  definitely lost: 16 bytes in 1 blocks
+	//  indirectly lost: 176 bytes in 4 blocks
+	//
+	// All above leaks (192 bytes) are caused by XGetDefault (in /usr/lib/libX11.so.6.3.0) - we have no control over this.
+	//
+	FIBITMAP* bitmap32;
+	if (bitsPerPixel == 32)
+	{
+		//std::cout << "Source image has " << bitsPerPixel << " bits per pixel. Skipping conversion." << std::endl;
+		bitmap32 = bitmap;
+	}
+	else
+	{
+		std::cout << "Source image has " << bitsPerPixel << " bits per pixel. Converting to 32-bit colour." << std::endl;
+		bitmap32 = FreeImage_ConvertTo32Bits(bitmap);
+	}
+
+	// Some basic image info - strip it out if you don't care
+	int imageWidth = FreeImage_GetWidth(bitmap32);
+	int imageHeight = FreeImage_GetHeight(bitmap32);
+	//std::cout << "Image: " << filename << " is size: " << imageWidth << "x" << imageHeight << "." << std::endl;
+
+	// Get a pointer to the texture data as an array of unsigned bytes.
+	// Note: At this point bitmap32 ALWAYS holds a 32-bit colour version of our image - so we get our data from that.
+	// Also, we don't need to delete or delete[] this textureData because it's not on the heap (so attempting to do
+	// so will cause a crash) - just let it go out of scope and the memory will be returned to the stack.
+	GLubyte* textureData = FreeImage_GetBits(bitmap32);
+
+	// Generate a texture ID and bind to it
+	GLuint tempTextureID;
+	glGenTextures(1, &tempTextureID);
+	glBindTexture(GL_TEXTURE_2D, tempTextureID);
+
+	// Construct the texture.
+	// Note: The 'Data format' is the format of the image data as provided by the image library. FreeImage decodes images into
+	// BGR/BGRA format, but we want to work with it in the more common RGBA format, so we specify the 'Internal format' as such.
+	glTexImage2D(GL_TEXTURE_2D,    // Type of texture
+		0,                // Mipmap level (0 being the top level i.e. full size)
+		GL_RGBA,          // Internal format
+		imageWidth,       // Width of the texture
+		imageHeight,      // Height of the texture,
+		0,                // Border in pixels
+		GL_BGRA,          // Data format
+		GL_UNSIGNED_BYTE, // Type of texture data
+		textureData);     // The image data to use for this texture
+
+						  // Specify our minification and magnification filters
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	// Check for OpenGL texture creation errors
+	GLenum glError = glGetError();
+	if (glError)
+	{
+		std::cout << "There was an error loading the texture: " << filename << std::endl;
+
+		switch (glError)
+		{
+		case GL_INVALID_ENUM:
+			std::cout << "Invalid enum." << std::endl;
+			break;
+
+		case GL_INVALID_VALUE:
+			std::cout << "Invalid value." << std::endl;
+			break;
+
+		case GL_INVALID_OPERATION:
+			std::cout << "Invalid operation." << std::endl;
+
+		default:
+			std::cout << "Unrecognised GLenum." << std::endl;
+			break;
+		}
+
+		std::cout << "See https://www.opengl.org/sdk/docs/man/html/glTexImage2D.xhtml for further details." << std::endl;
+	}
+
+	// Unload the 32-bit colour bitmap
+	FreeImage_Unload(bitmap32);
+
+	// If we had to do a conversion to 32-bit colour, then unload the original
+	// non-32-bit-colour version of the image data too. Otherwise, bitmap32 and
+	// bitmap point at the same data, and that data's already been free'd, so
+	// don't attempt to free it again! (or we'll crash).
+	if (bitsPerPixel != 32)
+	{
+		FreeImage_Unload(bitmap);
+	}
+
+	// Finally, return the texture ID
+	return tempTextureID;
+}
+
+
 bool defineVertexArrayObject() {
 
-	float color[] = {
-		1.0f, 0.0f, 0.0f, //vertex 1 : RED (1,0,0)
-		0.0f, 1.0f, 0.0f, //vertex 2 : GREEN (0,1,0) 
-		0.0f, 0.0f, 1.0f,  //vertex 3 : BLUE (0,0,1)
-		1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 1.0f,
-	};
+	//float color[] = {
+	//	1.0f, 0.0f, 0.0f, //vertex 1 : RED (1,0,0)
+	//	0.0f, 1.0f, 0.0f, //vertex 2 : GREEN (0,1,0) 
+	//	0.0f, 0.0f, 1.0f,  //vertex 3 : BLUE (0,0,1)
+	//	1.0f, 0.0f, 0.0f,
+	//	0.0f, 1.0f, 0.0f,
+	//	0.0f, 0.0f, 1.0f,
+	//	1.0f, 0.0f, 0.0f,
+	//	0.0f, 1.0f, 0.0f,
+	//	0.0f, 0.0f, 1.0f,
+	//};
+
+	/*float textureCoordinate[] = { 0.5f, 0.5f,
+								0.5f, 0.0f,
+								0.0f, 0.25f,
+								0.0f, 0.75f,
+								0.5f, 1.0f,
+								1.0f, 0.75f,
+								1.0f, 0.25f ,
+								0.5f, 0.0f};*/
+
+	float textureCoordinate[] = { 0.5f, 0.5f,
+							0.5f, 0.0f,
+							1.0f, 0.25f ,
+							1.0f, 0.75f,
+							0.5f, 1.0f,
+							0.0f, 0.75f,
+							0.0f, 0.25f,
+							0.5f, 0.0f };
 
 	//#2
 	//Vertex Buffer Object(VBO)를 생성하여 vertex 데이터를 복사한다.
@@ -39,9 +214,13 @@ bool defineVertexArrayObject() {
 	glBindBuffer(GL_ARRAY_BUFFER, mPositionVertexBufferObjectID);
 	glBufferData(GL_ARRAY_BUFFER, mMesh.GetPointCount() * sizeof(float) * 3, &mMesh.GetPoint()[0], GL_STATIC_DRAW);
 
-	glGenBuffers(1, &mColorVertexBufferObjectID);
-	glBindBuffer(GL_ARRAY_BUFFER, mColorVertexBufferObjectID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+	//glGenBuffers(1, &mColorVertexBufferObjectID);
+	//glBindBuffer(GL_ARRAY_BUFFER, mColorVertexBufferObjectID);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &triangleTextureCoordinateBufferObjectID);
+	glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(textureCoordinate), textureCoordinate, GL_STATIC_DRAW);
 
 	//#6
 	glGenVertexArrays(1, &mVertexArrayObject);
@@ -57,14 +236,23 @@ bool defineVertexArrayObject() {
 
 	glEnableVertexAttribArray(positionAttribute);
 
-	GLint colorAttribute = glGetAttribLocation(mShader.GetShaderID(), "colorAttribute");
-	if (colorAttribute == -1) {
-		std::cerr << "color 속성 설정 실패" << std::endl;
+	//GLint colorAttribute = glGetAttribLocation(mShader.GetShaderID(), "colorAttribute");
+	//if (colorAttribute == -1) {
+	//	std::cerr << "color 속성 설정 실패" << std::endl;
+	//	return false;
+	//}
+	//glBindBuffer(GL_ARRAY_BUFFER, mColorVertexBufferObjectID);
+	//glVertexAttribPointer(colorAttribute, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//glEnableVertexAttribArray(colorAttribute);
+
+	GLint textureCoordinateAttribute = glGetAttribLocation(mShader.GetShaderID(), "textureCoordinateAttribute");
+	if (textureCoordinateAttribute == -1) {
+		std::cerr << "Texture Coordinate 속성 설정 실패" << std::endl;
 		return false;
 	}
-	glBindBuffer(GL_ARRAY_BUFFER, mColorVertexBufferObjectID);
-	glVertexAttribPointer(colorAttribute, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(colorAttribute);
+	glBindBuffer(GL_ARRAY_BUFFER, triangleTextureCoordinateBufferObjectID);
+	glVertexAttribPointer(textureCoordinateAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(textureCoordinateAttribute);
 
 	glBindVertexArray(0);
 
@@ -233,6 +421,12 @@ int main()
 
 		glUseProgram(mShader.GetShaderID());
 		glBindVertexArray(mVertexArrayObject);
+
+		GLuint texureId = CreateTexture("assets\\image0.png");
+
+		glUniform1i(glGetUniformLocation(mShader.GetShaderID(), "tex"), 0);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texureId);
 
 		double currentTime = glfwGetTime();
 		numOfFrames++;
