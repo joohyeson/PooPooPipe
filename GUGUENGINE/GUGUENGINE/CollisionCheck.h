f1fe3bc06ca94e5de3bb43d9648915e5d94202e5
joohyeson <juhye.son990902@gmail.com>
2020-03-11 00:47:05 +0900

    Revert "collision hexa update"
    This reverts commit 872bb4424a4fcf7e6b2c559c2f32f716bed1fa37.

--- GUGUENGINE/GUGUENGINE/CollisionCheck.h
+++ GUGUENGINE/GUGUENGINE/CollisionCheck.h
@@ -10,18 +10,14 @@
 #include "Mathematics/Vector2.hpp"
 #include "Mesh.h"
 
-class CollisionCheck :public Component {
+class CollisionCheck :public Component{
 public:
-
 	CollisionCheck() :Component(COMPONENTTYOE_COLLISIONCHECK) {};
-	struct Edge;
 
-	bool Box2BoxCollision(Mesh* boxA, Mesh* boxB);
+	//bool Box2BoxCollision(Mesh* boxA, Mesh* boxB);
 	bool Point2BoxCollision(Vector2<float> point, Mesh* box);
-	bool Point2HexagonCollision(Vector2<float> point, Mesh* hexagon);
-
-	float GetDeterminant(Edge edge, Vector2<float> point);
-	float ShortestDistanceSqr(Vector2<float>& p, Edge& edge);
+	//bool Point2PointCollision(Vector2<float> pointA, Vector2<float> pointB);
+	//bool Point2HexagonCollision(Vector2<float> point, Mesh* hexagonCenter);
 
 	Vector2<float> GetMin(Mesh* mBox);   // bottom-left
 	Vector2<float> GetMax(Mesh* mBox);  // top-right
@@ -30,6 +26,3 @@ private:
 	bool isLive = false;
 	bool isCollide = false;
 };
-
-
-
