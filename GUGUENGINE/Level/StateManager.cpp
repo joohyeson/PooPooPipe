68884d3bae256a74249a7e60d3ecd6851940b65c
joohyeson <juhye.son990902@gmail.com>
2020-03-15 01:30:14 +0900

    Change state to mainmenu

--- GUGUENGINE/Level/StateManager.cpp
+++ GUGUENGINE/Level/StateManager.cpp
@@ -35,7 +35,7 @@ StateManager::~StateManager()
 
 void StateManager::Init()
 {
-	current = MOVINGCHECK;
+	current = MAINMENU;
 	
 	levels.push_back(new DigipenLogo());
 	levels.push_back(new FmodLogo());
